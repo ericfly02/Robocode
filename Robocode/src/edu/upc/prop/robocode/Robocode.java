8b96781ce0df9b2b7ebb3bce1d8cb16fdf9eddba
EricGd <speedyand@gmail.com>
2022-10-03 17:29:53 +0200

    corner test 1

--- Robocode/src/edu/upc/prop/robocode/Robocode.java
+++ Robocode/src/edu/upc/prop/robocode/Robocode.java
@@ -11,18 +11,49 @@ import robocode.*;
  */
 public class Robocode extends Robot{
 
-    public void run(){
-        turnLeft(getHeading());
-        while(true){
-            ahead(1000);
-            turnRight(90);
+    // Create a new robot which goes to the corner of the battlefield
+    // and then moves in a square pattern
+    public void run() {
+        // Set colors
+        setBodyColor(new Color(255, 0, 0));
+        setGunColor(new Color(0, 255, 0));
+        setRadarColor(new Color(0, 0, 255));
+        setBulletColor(new Color(255, 255, 0));
+        setScanColor(new Color(255, 0, 255));
+
+        // Loop forever
+        while (true) {
+            // Tell the game that when we take move,
+            // we'll also want to turn right... a lot.
+            setTurnRight(10000);
+            // Limit our speed to 5
+            setMaxVelocity(5);
+            // Start moving (and turning)
+            ahead(10000);
+            // Repeat.
         }
     }
+
+    // Create 5 robots
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            // Create a new robot
+            Robocode r = new Robocode();
+            // Set the robot colors
+
+        }
+    }
+
+
     
     public void onScannedRobot(ScannedRobotEvent e){
         fire(1);
     }
     
+    public void onHitByBullet(HitByBulletEvent e){
+        turnLeft(180);
+    }
+
     public void onHitByBullet(HitByBulletEvent e){
         turnLeft(180);
     }
